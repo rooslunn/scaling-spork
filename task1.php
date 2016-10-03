a07d952517b6c986f0cea4439df100061a77198c
Ruslan Kladko <rkladko@gmail.com>
2016-10-03 13:33:20 +0300

    Change cycle var name

--- task1.php
+++ task1.php
@@ -9,11 +9,11 @@ function parse_yandex_sms($sms) {
 
     $result = [];
 
-    foreach ($fields as $name => $pattern) {
-        $pattern = sprintf($pattern, $name);
-        $result[$name] = false;
+    foreach ($fields as $field => $pattern) {
+        $pattern = sprintf($pattern, $field);
+        $result[$field] = false;
         if (preg_match($pattern, $sms, $matches)) {
-            $result[$name] = $matches[$name];
+            $result[$field] = $matches[$field];
         }
     }
 
